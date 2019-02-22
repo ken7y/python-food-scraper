e7f7bf2b60858f4e3251451862354c3b9cf4ec81
ken7y <kenwang2227@gmail.com>
2019-02-22 12:28:05 +1100

    monkey code but working

--- loopscraper.py
+++ loopscraper.py
@@ -1,6 +1,15 @@
 import re
 import requests
 
+def CallingFunction(ingreds):
+    url = "https://www.food2fork.com/api/search?key=90913c69b33ea8fe0282a999efd60a44&q=TOBEREPLACED&page=1"
+    newUrl = re.sub('TOBEREPLACED',ingreds,url)
+    r = requests.get(newUrl)
+    for recipe in r.json()['recipes']:
+        # print(recipe)
+        for title in recipe['title']:
+            print(title, end="")
+        print("")
 
 class Node:
     def __init__(self, dataval=None):
@@ -22,7 +31,7 @@ class SLinkedList:
         while printval is not None:
             i+=1
             printval = printval.nextval
-        print(i)
+        return i
 
 print("Enter ingredients in this format aaaa,bbbb,cccc")
 ingredient = input("Enter ingredients:")
@@ -41,30 +50,36 @@ for ingreds in arr:
         node1.nextval = Node(ingreds)
         node1 = node1.nextval
         list1.lastval = node1
-list1.listsize()
+
+
+#All ingredients pull
+print("All the ingredients\n")
+CallingFunction(ingredient)
+print("\n\n")
+
+
+
+lenn = list1.listsize()
 
 # About to start the n-1 loops
+for x in range(list1.listsize()):
+    str = ""
+    listlength = lenn
+    node2 = list1.headval
+    while(listlength!=1):
+        str=str+node2.dataval + ","
+        listlength-=1;
+        node2 = node2.nextval
+    nodeTemp = list1.headval
+    list1.headval = list1.headval.nextval
+    list1.lastval.nextval = nodeTemp
+    list1.lastval = list1.lastval.nextval
+    str = str[:-1]
+    print ("These ingredients:" + str + "\n")
+    CallingFunction(str)
+    print("\n\n")
 
 #need to regex the ingredients to curl
 #need to store recipes
 #need to chop linkedlists
 #need to loop through chopped linked list
-
-
-
-
-
-
-# url = "https://www.food2fork.com/api/search?key=42c5f0fa1cf934475b5325034d15061f&q=TOBEREPLACED&page=1"
-#
-# newUrl = re.sub('TOBEREPLACED',ingredient,url)
-#
-# print(newUrl)
-# r = requests.get(newUrl)
-# # print(r.json())
-#
-# for recipe in r.json()['recipes']:
-#     # print(recipe)
-#     for title in recipe['title']:
-#         print(title, end="")
-#     print("")
